206de1792aba6d573a711bbff2625ccf151f5e4d
javierbrea <javier.brea@gmail.com>
2019-10-01 11:28:55 +0200

    Add origin _uniqueId unit tests

--- test/Origin.id.js
+++ test/Origin.id.js
@@ -19,23 +19,26 @@ test.describe("Origin id", () => {
     test.describe("without default value", () => {
       test.it("private property _id should be equal to given id", () => {
         const TestOrigin = class extends Origin {};
-        const testOrigin = new TestOrigin("foo-id");
+        const testOrigin = new TestOrigin(FOO_ID);
         test.expect(testOrigin._id).to.equal(FOO_ID);
       });
 
-      test.it("private property _id should be the hash of given id and undefined", () => {
+      test.it("private property _uniqueId should be the hash of given id and undefined", () => {
         const TestOrigin = class extends Origin {};
-        const testOrigin = new TestOrigin("foo-id");
+        const testOrigin = new TestOrigin(FOO_ID);
         test.expect(testOrigin._uniqueId).to.equal(hash(`${FOO_ID}${undefined}`));
       });
     });
 
     test.describe("with default value", () => {
-      test.it("private property _id should be the hash of given id and undefined", () => {
-        const TestOrigin = class extends Origin {};
-        const testOrigin = new TestOrigin("foo-id", []);
-        test.expect(testOrigin._uniqueId).to.equal(hash(`${FOO_ID}${JSON.stringify([])}`));
-      });
+      test.it(
+        "private property _uniqueId should be the hash of given id and given default value",
+        () => {
+          const TestOrigin = class extends Origin {};
+          const testOrigin = new TestOrigin(FOO_ID, []);
+          test.expect(testOrigin._uniqueId).to.equal(hash(`${FOO_ID}${JSON.stringify([])}`));
+        }
+      );
     });
   });
 
@@ -44,10 +47,27 @@ test.describe("Origin id", () => {
       "private property _id should be equal to the combination of given id and the queryId",
       () => {
         const TestOrigin = class extends Origin {};
-        const testOrigin = new TestOrigin("foo-id").query({
+        const testOrigin = new TestOrigin(FOO_ID).query({
           foo: "foo-query"
         });
         test.expect(testOrigin._id).to.equal(`${FOO_ID}-{"foo":"foo-query"}`);
+      }
+    );
+
+    test.it(
+      "private property _uniqueId should be the hash of root _uniqueId and given query",
+      () => {
+        const TestOrigin = class extends Origin {};
+        const testOrigin = new TestOrigin(FOO_ID, []).query({
+          foo: "foo-query"
+        });
+        test.expect(testOrigin._uniqueId).to.equal(
+          hash(
+            `${hash(`${FOO_ID}${JSON.stringify([])}`)}${JSON.stringify({
+              foo: "foo-query"
+            })}`
+          )
+        );
       }
     );
   });
@@ -57,7 +77,7 @@ test.describe("Origin id", () => {
       "private property _id should be equal to the combination of given id and the extension of the queries ids",
       () => {
         const TestOrigin = class extends Origin {};
-        const testOrigin = new TestOrigin("foo-id")
+        const testOrigin = new TestOrigin(FOO_ID)
           .query({
             foo: "foo-query"
           })
@@ -65,6 +85,28 @@ test.describe("Origin id", () => {
             foo2: "foo-query-2"
           });
         test.expect(testOrigin._id).to.equal(`${FOO_ID}-{"foo":"foo-query","foo2":"foo-query-2"}`);
+      }
+    );
+
+    test.it(
+      "private property _uniqueId should be equal to the combination of root _uniqueId and the extension of the queries ids",
+      () => {
+        const TestOrigin = class extends Origin {};
+        const testOrigin = new TestOrigin(FOO_ID, [])
+          .query({
+            foo: "foo-query"
+          })
+          .query({
+            foo2: "foo-query-2"
+          });
+        test.expect(testOrigin._uniqueId).to.equal(
+          hash(
+            `${hash(`${FOO_ID}${JSON.stringify([])}`)}${JSON.stringify({
+              foo: "foo-query",
+              foo2: "foo-query-2"
+            })}`
+          )
+        );
       }
     );
   });
